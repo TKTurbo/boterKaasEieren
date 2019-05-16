c2d1da75eb8d6d50799efeeacdb5db75c685699d
Thomas <thomaskreder12345@gmail.com>
2019-05-16 22:08:39 +0200

    bruh

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+/*
+### # # ###         #
+ #  # #  #  # # ### ### ###
+ #  ##   #  # # #   # # # #
+ #  # #  #  ### #   ### ###
+ #  # #  #
+*/
+
+var players = [];
+var cells = [];
+
 window.onload = function(){ // Als de pagina volledig is geladen zet dan deze variabelen
 
-    var cells = document.getElementsByTagName('td');
+    cells = document.getElementsByTagName('td');
+    players = [document.getElementById("speler1"), document.getElementById("speler2")]; // De headers met namen van de spelers
 
     var clickedCells = [0, 1]; // 0 is speler 1, 1 is speler 2
     clickedCells[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0]; //Alle kaarten in het spel, op volgorder van index
@@ -10,14 +22,17 @@ window.onload = function(){ // Als de pagina volledig is geladen zet dan deze va
 function startGame(){ // Nadat er geklikt is op de startknop
     var speler1 = prompt('Naam speler 1');
     var speler2 = prompt('Naam speler 2');
+    players[0].innerHTML = speler1;
+    players[1].innerHTML = speler2;
 
-    var firstPlayer = alert(Math.floor(Math.random() * 2) + 1); // 1 of 2
+    var firstPlayer = Math.floor(Math.random() * 2) + 1; // 1 of 2
+
+    for(var i = 0; i < cells.length; i++){
+        cells[i].addEventListener('click', clickOnCell)
+    }
 }
 
 function clickOnCell($hokjeID){
-    if(hokje1 == 1){
-
-    }
 }
 
 function resetEverything(){
