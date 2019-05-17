15b70c9e643cca70dd362a9b4821b047bcea6e83
TKTurbo <thomaskreder12345@gmail.com>
2019-05-17 23:54:44 +0200

    probeerselwerkt

--- main.js
+++ main.js
@@ -10,6 +10,7 @@
 
 var players = [];
 var cells = [];
+var clickedCells = [];
 var currentPlayer;
 
 window.onload = function(){ // Als de pagina volledig is geladen zet dan deze variabelen
@@ -20,9 +21,14 @@ window.onload = function(){ // Als de pagina volledig is geladen zet dan deze va
     var clickedCells = [0, 1]; // 0 is speler 1, 1 is speler 2
     clickedCells[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0]; //Alle kaarten in het spel, op volgorder van index
     clickedCells[1] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    document.getElementById('gameButton').addEventListener('click', startGame);
 };
 
 function startGame(){ // Nadat er geklikt is op de startknop
+    document.getElementById('gameButton').removeEventListener('click', startGame);
+    document.getElementById('gameButton').innerHTML = 'Reset';
+    document.getElementById('gameButton').addEventListener('click', reloadPage);
     var speler1 = prompt('Naam speler 1');
     var speler2 = prompt('Naam speler 2');
     players[0].innerHTML = speler1 + ' = O';
@@ -36,16 +42,20 @@ function startGame(){ // Nadat er geklikt is op de startknop
 function clickOnCell(event){
     if(currentPlayer === 1){
         cells[event.target.id].innerHTML = 'O';
-    }else if(currentPlayer === 2){;
+        checkEnd();
+        currentPlayer = 2;
+    }else if(currentPlayer === 2){
         cells[event.target.id].innerHTML = 'X';
+        checkEnd();
+        currentPlayer = 1;
     }
 }
 
-function resetEverything(){
-    clickedCells[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    clickedCells[1] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-    //TODO: Score reset en andere dingen
+function reloadPage(){
+    var quit = confirm('Zeker weten?');
+    if(quit === true){
+        location.reload();
+    }
 }
 
 function enableClicking(){
@@ -66,4 +76,32 @@ function checkClicked(event){ // Kijkt of er al op de knop geklikt is
     }else{
         return false;
     }
+}
+
+function checkEnd(){
+    for(var i = 0; i < cells.length; i++){
+        clickedCells[i] = cells[i].innerHTML; // Array met alle gelikte cellen en
+    }
+
+    if(clickedCells[0] && clickedCells[1] && clickedCells[2] === 'X' // TODO: Refactoring?
+        || clickedCells[3] && clickedCells[4] && clickedCells[5] === 'X'
+        || clickedCells[6] && clickedCells[7] && clickedCells[8] === 'X'
+        || clickedCells[0] && clickedCells[3] && clickedCells[6] === 'X'
+        || clickedCells[1] && clickedCells[4] && clickedCells[7] === 'X'
+        || clickedCells[2] && clickedCells[5] && clickedCells[8] === 'X'
+        || clickedCells[0] && clickedCells[4] && clickedCells[7] === 'X'
+        || clickedCells[2] && clickedCells[4] && clickedCells[6] === 'X'){
+        alert(currentPlayer + ' heeft gewonnen!')
+    }else if(clickedCells[0] && clickedCells[1] && clickedCells[2] === 'O' // TODO: Refactoring?
+        || clickedCells[3] && clickedCells[4] && clickedCells[5] === 'O'
+        || clickedCells[6] && clickedCells[7] && clickedCells[8] === 'O'
+        || clickedCells[0] && clickedCells[3] && clickedCells[6] === 'O'
+        || clickedCells[1] && clickedCells[4] && clickedCells[7] === 'O'
+        || clickedCells[2] && clickedCells[5] && clickedCells[8] === 'O'
+        || clickedCells[0] && clickedCells[4] && clickedCells[7] === 'O'
+        || clickedCells[2] && clickedCells[4] && clickedCells[6] === 'O') {
+        alert(currentPlayer + ' heeft gewonnen!')
+    }
+
+    // 012, 345, 678, 036, 147, 258, 047, 246 - Alle mogelijke combinaties
 }
